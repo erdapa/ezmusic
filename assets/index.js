a5bfd58bfff8aa95796aa19b2dfac7be7c864fb9
Eric Payne <duncecapsoftware@gmail.com>
2017-09-19 12:42:25 -0700

    keep

---
+++ assets/index.js
@@ -0,0 +1,47 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "228px";
+    document.getElementById("main").style.marginLeft = "228px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
+}
+
+function openAbout() {
+    document.getElementById("aboutSidenav").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+    document.body.style.backgroundColor = "#000";
+}
+
+function closeAbout() {
+    document.getElementById("aboutSidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#000";
+}
+
+function openContact() {
+    document.getElementById("contactSidenav").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+    document.body.style.backgroundColor = "#000";
+}
+
+function closeContact() {
+    document.getElementById("contactSidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#000";
+}
+function openDownload() {
+    document.getElementById("downloadSidenav").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+    document.body.style.backgroundColor = "#000";
+}
+
+function closeDownload() {
+    document.getElementById("downloadSidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#000";
+}
